3c2ad50e725dc2ea4467724151d1828efd68c822
Matthew <mrgarelli@gmail.com>
2019-03-03 22:11:27 -0500

    added a few python checks

---
+++ python/checks/thisOrThat.py
@@ -0,0 +1,7 @@
+# hp = -1
+hp = 50
+
+outOfBounds = (hp<=0, hp >= 50)
+
+if any(outOfBounds):
+	print('true')
---
+++ python/loop/arrayOfTyples.py
@@ -0,0 +1,4 @@
+list = [(0, 1), (2, 3), (4, 5)]
+
+for (first, second) in list:
+	print(first, second)

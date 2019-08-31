74c9320994cd6a5d78d9c9c627fc69530a5a6d23
pablo <mgarelli@alumni.stanford.edu>
2019-08-30 21:09:52 -0400

    tabbed print now using lambda

--- python/decorators/tabbed_print.py
+++ python/decorators/tabbed_print.py
@@ -1,7 +1,5 @@
 def _tabbed_print_decorator(function_to_decorate):
-	def wrapper(*args, **kwargs):
-		function_to_decorate('\t', *args, **kwargs)
-	return wrapper
+	return lambda *args, **kwargs: function_to_decorate('\t', *args, **kwargs)
 
 @_tabbed_print_decorator
 def t_print(*args, **kwargs): print(*args, **kwargs)

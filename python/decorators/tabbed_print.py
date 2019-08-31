15bf08ae092fa83fa79021d2e22bda4c7b81a712
pablo <mgarelli@alumni.stanford.edu>
2019-08-30 21:20:06 -0400

    tabbed print turned out to not be a decorator, but useful nonetheless, method types example completed

--- python/decorators/tabbed_print.py
+++
@@ -1,7 +0,0 @@
-def _tabbed_print_decorator(function_to_decorate):
-	return lambda *args, **kwargs: function_to_decorate('\t', *args, **kwargs)
-
-@_tabbed_print_decorator
-def t_print(*args, **kwargs): print(*args, **kwargs)
-
-t_print('dance')
--- python/methods/method_types.py
+++ python/methods/method_types.py
@@ -1,3 +1,5 @@
+def t_print(*args, **kwargs): print('\t', *args, **kwargs)
+
 class DecoratorExample:
 	def __init__(self):
 		""" Example Setup """
@@ -5,9 +7,9 @@ class DecoratorExample:
 
 	@staticmethod
 	def _private_static_method():
-		print('I\'m a private static method!')
-		print('Didn\'t need any attributes for this')
-		print('Also only accessible within this class')
+		t_print('I\'m a private static method!')
+		t_print('Didn\'t need any attributes for this')
+		t_print('Also only accessible within this class')
  
 	@staticmethod
 	def static_method():
@@ -17,7 +19,14 @@ class DecoratorExample:
 	def instance_method(self):
 		print('I\'m an instance method!')
 		print('I can access attributes for example, self.name is: ' + self.name)
-		print('I can also call other class methods: ', self._private_static_method())
+		print('I can also call other class methods: ')
+		self._private_static_method()
+
+	@classmethod
+	def class_method(cls):
+		print('I\'m a class method!')
+		print('I can only call other class methods: ')
+		cls._private_static_method()
 
 DecoratorExample.static_method()
 print()
@@ -25,3 +34,5 @@ print()
 de = DecoratorExample()
 de.instance_method()
 print()
+
+de.class_method()

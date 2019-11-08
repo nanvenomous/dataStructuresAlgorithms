58aedda0f5203bbe42dbaa4384716056a98c0a03
pablo <mgarelli@alumni.stanford.edu>
2019-11-08 11:48:16 -0500

    added java section for leet code Two sum solution for negative numbers

---
+++ java/leetcode/TwoSum.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+// A Class that represents use-defined expception 
+class TwoSum { 
+  public int[] twoSum(int[] nums, int target) {
+    // Solution assuming no negative numbers
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] <= target) {
+        for (int j = i + 1; j < nums.length; j++) {
+          if (nums[i] + nums[j] == target) {
+            int ans[] = new int[]{i, j};
+            return ans;
+          }
+        }
+      }
+    }
+    return new int[2];
+  } 
+  public static void main(String args[]) { 
+    // int[] nums = {2, 7, 11, 15};
+    int[] nums = {0, 4, 3, 0};
+    int target = 0;
+    TwoSum sln = new TwoSum();
+    int[] ans = sln.twoSum(nums, target);
+    System.out.println(Arrays.toString(ans));
+  } 
+}; 

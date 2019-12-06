4970c3f50249fb1be91b8f016f8dec110e8d4ae0
pablo <mgarelli@alumni.stanford.edu>
2019-12-05 22:40:28 -0500

    now reversing integer based on all tests

--- java/leetcode/java_lib/src/main/java/java_lib/ReverseInteger/ReverseInteger.java
+++ java/leetcode/java_lib/src/main/java/java_lib/ReverseInteger/ReverseInteger.java
@@ -1,9 +1,14 @@
 package java_lib.ReverseInteger;
 
 class ReverseInteger {
-    public int run(int forward) {
-        int reversed;
-        reversed = forward;
-        return reversed;
-    }
-};
+	public int run(int forward) {
+		int reversed = 0;
+
+		while (forward != 0) {
+			reversed *= 10;
+			reversed += forward % 10;
+			forward = forward / 10;
+		}
+		return reversed;
+	}
+}
--- java/leetcode/java_lib/src/test/java/java_lib/ReverseInteger/ReverseIntegerTest.java
+++ java/leetcode/java_lib/src/test/java/java_lib/ReverseInteger/ReverseIntegerTest.java
@@ -1,36 +1,48 @@
 package java_lib.ReverseInteger;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReverseIntegerTest {
-
-    //    private TwoSum_HashTableTwoPass subject;
     private ReverseInteger subject;
 
     @BeforeEach
     public void setup() {
-//        subject = new TwoSum_HashTableTwoPass();
         subject = new ReverseInteger();
     }
 
-    private void assertReversesInteger(int forward, int reverse) {
+    private void assertReversesInteger(int reverse, int forward) {
         int ans = subject.run(forward);
         assertEquals(ans, reverse);
+    }
+
+    @Test
+    public void should_reverse() {
+        int forward = 123;
+        int reverse = 321;
+        assertReversesInteger(reverse, forward);
     }
 
     @Test
     public void should_reverse_with_negative() {
         int forward = -123;
         int reverse = -321;
-        assertReversesInteger(forward, reverse);
+        assertReversesInteger(reverse, forward);
     }
 
     @Test
     public void should_reverse_with_zero() {
         int forward = 120;
         int reverse = 21;
-        assertReversesInteger(forward, reverse);
+        assertReversesInteger(reverse, forward);
+    }
+
+    @Test
+    public void should_reverse_zero() {
+        int forward = 0;
+        int reverse = 0;
+        assertReversesInteger(reverse, forward);
     }
 }

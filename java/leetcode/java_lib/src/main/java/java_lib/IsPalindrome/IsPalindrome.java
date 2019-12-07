c491dde1779c43c340559e5e24d66c093f9b39a3
pablo <mgarelli@alumni.stanford.edu>
2019-12-07 14:49:43 -0500

    made forward an attribute

--- java/leetcode/java_lib/src/main/java/java_lib/IsPalindrome/IsPalindrome.java
+++ java/leetcode/java_lib/src/main/java/java_lib/IsPalindrome/IsPalindrome.java
@@ -3,30 +3,34 @@ package java_lib.IsPalindrome;
 class IsPalindrome {
 	public boolean isEven;
 	int numDigits;
+	int forward;
 
 	public boolean run(int x) {
 		if (x < 0) {return false;}
+		forward = x;
 		return false;
 	}
 
-	public void digits_to_reverse(int pal) {
-		numDigits = (int)Math.log10(pal);
+	public void digits_to_reverse() {
+		numDigits = (int)Math.log10(forward);
 		isEven = !(0 == numDigits % 2);
 		if (isEven) {numDigits += 1;}
 		numDigits /= 2;
 	}
 
-	public int partial_reverse(int forward) {
+	public int partial_reverse() {
 		int reversed = 0;
 
-		while (forward != 0) {
-			reversed = safe_increment_reversed(reversed, forward);
+		digits_to_reverse();
+		while (numDigits > 0) {
+			reversed = safe_increment_reversed(reversed);
 			forward /= 10;
+			numDigits--;
 		}
 		return reversed;
 	}
 
-	public int safe_increment_reversed(int reversed, int forward) {
+	public int safe_increment_reversed(int reversed) {
 		reversed *= 10;
 		reversed += forward % 10;
 		return reversed;
--- java/leetcode/java_lib/src/test/java/java_lib/IsPalindrome/IsPalindromeTest.java
+++ java/leetcode/java_lib/src/test/java/java_lib/IsPalindrome/IsPalindromeTest.java
@@ -8,87 +8,87 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class IsPalindromeTest {
-    private IsPalindrome subject;
+	private IsPalindrome subject;
 
-    @BeforeEach
-    public void setup() {
-        subject = new IsPalindrome();
-    }
+	@BeforeEach
+	public void setup() {
+		subject = new IsPalindrome();
+	}
 
-    @Test
-    public void should_get_correct_digits_to_reverse_odd() {
-        int num = 1234321;
-		  int expected = 3;
-        subject.digits_to_reverse(num);
-		  assertFalse(subject.isEven);
-        assertEquals(expected, subject.numDigits);
-    }
+	private void assert_determines_digit_number_and_eveness(int num, int numDigits, boolean eveness) {
+		subject.forward = num;
+		subject.digits_to_reverse();
+		if (eveness) { assertTrue(subject.isEven);
+		} else { assertFalse(subject.isEven);}
+		assertEquals(numDigits, subject.numDigits);
+	}
 
-    @Test
-    public void should_get_correct_digits_to_reverse_even() {
-        int num = 123321;
-		  int expected = 3;
-        subject.digits_to_reverse(num);
-		  assertTrue(subject.isEven);
-        assertEquals(expected, subject.numDigits);
-    }
+	@Test
+	public void should_get_correct_digits_to_reverse_odd() {
+		int num = 1234321;
+		int numDigits = 3;
+		boolean eveness = false;
+		assert_determines_digit_number_and_eveness(num, numDigits, eveness);
+	}
 
-    private void assert_determines_palindrome(int num, boolean isPal) {
-        boolean ans = subject.run(num);
-        assertEquals(isPal, ans);
-    }
+	@Test
+	public void should_get_correct_digits_to_reverse_even() {
+		int num = 123321;
+		int numDigits = 3;
+		boolean eveness = true;
+		assert_determines_digit_number_and_eveness(num, numDigits, eveness);
+	}
 
-    @Test
-    public void should_determine_is_not_palindrome() {
-        int num = 123456;
-        boolean isPal = false;
-        assert_determines_palindrome(num, isPal);
-    }
+	private void assert_determines_palindrome(int num, boolean isPal) {
+		boolean ans = subject.run(num);
+		assertEquals(isPal, ans);
+	}
 
-    @Test
-    public void should_determine_is_palindrome() {
-        int num = 1234321;
-        boolean isPal = true;
-        assert_determines_palindrome(num, isPal);
-    }
+	@Test
+	public void should_determine_is_not_palindrome() {
+		int num = 123456;
+		boolean isPal = false;
+		assert_determines_palindrome(num, isPal);
+	}
 
-    @Test
-    public void should_classify_negative_as_not_a_palindrome() {
-        int num = -1234321;
-        boolean isPal = false;
-        assert_determines_palindrome(num, isPal);
-    }
+	@Test
+	public void should_determine_is_palindrome() {
+		int num = 1234321;
+		boolean isPal = true;
+		assert_determines_palindrome(num, isPal);
+	}
 
-    private void assertReversesInteger(int reverse, int forward) {
-        int ans = subject.partial_reverse(forward);
-        assertEquals(reverse, ans);
-    }
+	@Test
+	public void should_classify_negative_as_not_a_palindrome() {
+		int num = -1234321;
+		boolean isPal = false;
+		assert_determines_palindrome(num, isPal);
+	}
 
-    @Test
-    public void should_reverse_part_even() {
-        int forward = 999321;
-        int reverse = 123;
-        assertReversesInteger(reverse, forward);
-    }
+	private void assertReversesInteger(int reverse, int forward) {
+		subject.forward = forward;
+		int ans = subject.partial_reverse();
+		assertEquals(reverse, ans);
+	}
 
-    @Test
-    public void should_reverse_with_negative() {
-        int forward = -123;
-        int reverse = -321;
-        assertReversesInteger(reverse, forward);
-    }
+	@Test
+	public void should_reverse_part_even() {
+		int forward = 999321;
+		int reverse = 123;
+		assertReversesInteger(reverse, forward);
+	}
 
-    @Test
-    public void should_reverse_with_zero() {
-        int forward = 120;
-        int reverse = 21;
-        assertReversesInteger(reverse, forward);
-    }
+	@Test
+	public void should_reverse_with_zero() {
+		int forward = 9991027;
+		int reverse = 720;
+		assertReversesInteger(reverse, forward);
+	}
 
-    @Test
-    public void should_reverse_zero() {
-        int forward = 0;
-        int reverse = 0;
-        assertReversesInteger(reverse, forward);
-    }
+	@Test
+	public void should_reverse_zero() {
+		int forward = 0;
+		int reverse = 0;
+		assertReversesInteger(reverse, forward);
+	}
 }

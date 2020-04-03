c5009d463d1cfcdb7561e21ac42802910af9ed2d
Matthew Garelli <mrgarelli@gmail.com>
2020-04-02 23:56:38 -0400

    brute force working happy number

---
+++ python/math/happy_numbers/happy_number.py
@@ -0,0 +1,50 @@
+class HappyNumber:
+	def __init__(self, cur=None, next=None):
+		self.cur = cur
+		self.next = next
+		self.possible_repeat = False
+		self.history = []
+		self.i1 = 0
+		self.i2 = 0
+		self.repeat_index = None
+
+	def add_squared_digits(self):
+		self.next = 0
+		while self.cur != 0:
+			self.next += (self.cur % 10)**2
+			self.cur = int(self.cur/10)
+
+	def _still_repeating(self):
+		return self.history[self.i1 + self.repeat_index] == self.history[self.i2 + self.repeat_index]
+
+	def _fully_repeated(self):
+		return self.i1 + self.repeat_index == self.i2
+
+	def loop_repeats_once(self):
+		if not self.possible_repeat:
+			for (i, elem) in enumerate(self.history[:-1]):
+				if elem == self.history[-1]:
+					self.possible_repeat = True
+					self.i1 = i
+					self.i2 = len(self.history) - 1
+					self.repeat_index = 1
+		else: # possible repeat
+			if self._still_repeating():
+				if self._fully_repeated():
+					return True
+			else: self.possible_repeat = False
+			self.repeat_index += 1
+		return False
+
+	def isHappy(self, n: int) -> bool:
+		self.cur = n
+		self.possible_repeat = False
+		self.history = []
+		self.i1 = 0
+		self.i2 = 0
+		while self.cur != 1:
+			self.history.append(self.cur)
+			self.add_squared_digits()
+			self.cur = self.next
+			if self.loop_repeats_once(): return False
+		return True
---
+++ python/math/happy_numbers/tests/test_happy_number.py
@@ -0,0 +1,60 @@
+import pytest
+from ..happy_number import HappyNumber
+
+class TestHappyNumber:
+
+	@classmethod
+	def setup_method(cls):
+		cls.subject = HappyNumber()
+
+	def test_identifies_happy_number(self):
+		assert True == self.subject.isHappy(19)
+
+	def test_identifies_unhappy_number(self):
+		assert False == self.subject.isHappy(15)
+
+class TestLoopRepeatsOnce:
+
+	@classmethod
+	def setup_method(cls):
+		cls.subject = HappyNumber()
+		cls.subject.possible_repeat = False
+		cls.subject.history = []
+		cls.subject.i1 = 0
+		cls.subject.i2 = 0
+
+	def test_finds_repeat(self):
+		mock_squares = [5, 7, 6, 3, 2, 6, 3, 2, 6]
+		for (i, num) in enumerate(mock_squares):
+			self.subject.history.append(num)
+			if i == len(mock_squares) - 1: assert True == self.subject.loop_repeats_once()
+			else: assert False == self.subject.loop_repeats_once()
+			if i <= 4: assert self.subject.possible_repeat == False
+			else: assert self.subject.possible_repeat == True
+
+	def test_resets_repeat(self):
+		mock_squares = [5, 7, 6, 3, 2, 6, 3, 4]
+		for (i, num) in enumerate(mock_squares):
+			self.subject.history.append(num)
+			assert False == self.subject.loop_repeats_once()
+			if i <= 4: assert self.subject.possible_repeat == False
+			elif i <= 6: assert self.subject.possible_repeat == True
+			else: assert self.subject.possible_repeat == False
+
+class TestAddSquaredDigits:
+
+	@classmethod
+	def setup_method(cls):
+		cls.subject = HappyNumber()
+
+	def test_squares_all_digits_and_sums(self):
+		self.subject.cur = 324
+		self.subject.add_squared_digits()
+		assert self.subject.next == 3**2 + 2**2 + 4**2
+		assert self.subject.cur == 0
+
+	def test_squares_all_digits_with_many_zeros_and_sums(self):
+		self.subject.cur = 1000
+		self.subject.add_squared_digits()
+		assert self.subject.next == 1
+		assert self.subject.cur == 0

60371ccee52c31cea90179b561a437b8c1d7adb5
Matthew Garelli <mrgarelli@gmail.com>
2021-01-28 01:09:15 -0600

    MG | text justification working for examples 1, 2, 3

--- python/leetcode/text_justification/tests/test_text_justification.py
+++ python/leetcode/text_justification/tests/test_text_justification.py
@@ -6,6 +6,12 @@ class mocks:
     class one: 
         words = ['This', 'is', 'an', 'example', 'of', 'text', 'justification.']
         maxWidth = 16
+    class two:
+        words = ['What', 'must', 'be', 'acknowledgment', 'shall', 'be']
+        maxWidth = 16
+    class three:
+        words = ['Science', 'is', 'what', 'we', 'understand', 'well', 'enough', 'to', 'explain', 'to', 'a', 'computer.', 'Art', 'is', 'everything', 'else', 'we', 'do']
+        maxWidth = 20
 
 class answers:
     one = [
@@ -13,14 +19,36 @@ class answers:
             'example  of text',
             'justification.  ',
             ]
+    two = [
+            'What   must   be',
+            'acknowledgment  ',
+            'shall be        ',
+            ]
+    three = [
+            'Science  is  what we',
+            'understand      well',
+            'enough to explain to',
+            'a  computer.  Art is',
+            'everything  else  we',
+            'do                  ',
+            ]
 
 class TestTextJustification:
     @classmethod
     def setup_method(cls):
         cls.subject = TextJustification()
 
-    # def test_example(self):
-    #     assert self.subject.fullJustify(mocks.one.words, mocks.one.maxWidth) == 0
+    def assert_justifies_text(self, mock, ans):
+        assert self.subject.fullJustify(mock.words, mock.maxWidth) == ans
+
+    def test_mock_one(self):
+        self.assert_justifies_text(mocks.one, answers.one)
+
+    def test_mock_two(self):
+        self.assert_justifies_text(mocks.two, answers.two)
+
+    def test_mock_three(self):
+        self.assert_justifies_text(mocks.three, answers.three)
 
     def test_get_line(self):
         self.subject.words = mocks.one.words
--- python/leetcode/text_justification/text_justification.py
+++ python/leetcode/text_justification/text_justification.py
@@ -1,40 +1,63 @@
-# nOpeneins = 3
-# extraSpaces = 7
-# [' ', ' ', ' '] 
-
 from typing import List
 class TextJustification:
     def __init__(self) -> None:
         self.words = None
         self.maxWidth = 0
+        self.finished = False
 
     def _getLine(self, s, e, curLineLen):
         words = self.words[s:e]
-        nWords = len(words)
-        nOpenings = nWords - 1
-        extraSpaces = self.maxWidth - curLineLen
-        evenSpaces = int(extraSpaces/nOpenings) + 1
-        additionalSpaces = extraSpaces%nOpenings
-        line = []
-        for i in range(nOpenings):
-            line.append(words[i])
-            width = evenSpaces + 1 if additionalSpaces else evenSpaces
+        # left justify
+        if self.finished:
+            lineLst = []
+            for wd in words:
+                lineLst += [wd, ' ']
+            lineLst.pop()
+            width = (self.maxWidth - curLineLen)
             emptySpace = [' ' for _ in range(width)]
-            line += emptySpace
-            if additionalSpaces > 0: additionalSpaces -= 1
-        line.append(words[i + 1])
-        return ''.join(line)
+            lineLst += emptySpace
+            return ''.join(lineLst)
+        nWords = len(words)
+        if not nWords == 1:
+            nOpenings = nWords - 1
+            extraSpaces = self.maxWidth - curLineLen
+            evenSpaces = int(extraSpaces/nOpenings) + 1
+            additionalSpaces = extraSpaces%nOpenings
+            line = []
+            for i in range(nOpenings):
+                line.append(words[i])
+                width = evenSpaces + 1 if additionalSpaces else evenSpaces
+                emptySpace = [' ' for _ in range(width)]
+                line += emptySpace
+                if additionalSpaces > 0: additionalSpaces -= 1
+            line.append(words[i + 1])
+            return ''.join(line)
+        else:
+            word = words[0]
+            remainingCharacters = self.maxWidth - len(word)
+            emptySpace = [' ' for _ in range(remainingCharacters)]
+            words += emptySpace
+            return ''.join(words)
 
     def _measureLine(self, s):
         curLineLen = 0
-        potLineLen = len(self.words[s])
+        if not s > len(self.words) - 1:
+            potLineLen = len(self.words[s])
+        else:
+            self.finished = True
+            return
         potEnd = s + 1
         # ensure potential length is not more than maximum allowed length
-        while potLineLen < self.maxWidth:
+        while potLineLen <= self.maxWidth:
             curLineLen = potLineLen
-            # potLineLen is current line length + a space and the length of the next word
-            potLineLen = curLineLen + 1 + len(self.words[potEnd])
-            potEnd += 1
+            if not potEnd > len(self.words) - 1:
+                # potLineLen is current line length + a space and the length of the next word
+                potLineLen = curLineLen + 1 + len(self.words[potEnd])
+                potEnd += 1
+            else:
+                self.finished = True
+                potEnd += 1
+                break
         # make the line with the gathered information
         line = self._getLine(
             s=s,
@@ -46,11 +69,11 @@ class TextJustification:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         self.words = words
         self.maxWidth = maxWidth
+        justifiedText = []
         s = 0
-        line, i = self._measureLine(
-            s=s
-            )
-        print()
-        print(line)
-        print(i)
-        return 0
+        while not self.finished:
+            line, s = self._measureLine(
+                s=s
+                )
+            justifiedText.append(line)
+        return justifiedText

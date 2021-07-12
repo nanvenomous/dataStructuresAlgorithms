121dc869051c132c02eccb85ffe06baebd2b1058
Matthew Garelli <mrgarelli@gmail.com>
2021-07-11 21:05:07 -0600

    MG | best sum has memoized solution now

--- python/leetcode/best_sum/best_sum.py
+++ python/leetcode/best_sum/best_sum.py
@@ -11,10 +11,16 @@
 2 - 2 = 0
 '''
 
+# m is size of target
+# n is length of numbers
+# normal time complexity is n^m * m
+# memoized time complexity n * m^2
+
 class BestSum:
-	def __init__(self): pass
+	def __init__(self): self.memo = {}
 
 	def bestSum(self, numbers, target):
+		if target in self.memo: return self.memo[target]
 		if target == 0: return []
 		if target < 0: return None
 		shortest = None
@@ -26,8 +32,19 @@ class BestSum:
 				nxtSolution = subSolution + [n]
 				if (shortest == None) or (len(nxtSolution) < len(shortest)):
 					shortest = nxtSolution
+		self.memo[target] = shortest
 		return shortest
 
-# m is size of target
-# n is length of numbers
-# normal time complexity is n^m
+	def bestSumBruteForce(self, numbers, target):
+		if target == 0: return []
+		if target < 0: return None
+		shortest = None
+		for n in numbers: # find sub sums
+			rem = target - n
+
+			subSolution = self.bestSumBruteForce(numbers, rem)
+			if subSolution != None:
+				nxtSolution = subSolution + [n]
+				if (shortest == None) or (len(nxtSolution) < len(shortest)):
+					shortest = nxtSolution
+		return shortest
